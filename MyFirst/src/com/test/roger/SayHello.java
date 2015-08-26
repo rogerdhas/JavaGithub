ec4a8ce9a61129c4d867bb65ab2829055440b9ac
rogerdhas <rogerdhas@gmail.com>
2015-08-26 08:21:06 +0530

    test

---
+++ MyFirst/src/com/test/roger/SayHello.java
@@ -0,0 +1,12 @@
+package com.test.roger;
+
+public class SayHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My first java app");
+
+	}
+
+}
